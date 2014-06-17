9edac2f948a19b8f42052b5bac6be0d9b166f5d3
bharathios <bharathn.ios@gmail.com>
2014-06-17 12:23:18 +0530

    Initial Commit

---
+++ FIFASwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FIFASwift
+//
+//  Created by Bharath Nagaraj Rao on 17/06/14.
+//  Copyright (c) 2014 Bharath Nagaraj Rao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
